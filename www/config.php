96cac419c7d967d5ac5c808804f8d5446d28e018
Nigel Johnson <nigel@nigeljohnson.net>
2021-09-07 16:40:58 +0100

    Found better host for app engine

--- www/config.php
+++ www/config.php
@@ -1,10 +1,12 @@
 <?php
 $localdev_namespace = "localdev"; // Override this in config_override.php for a competely blank area of your own
 $project_id = "coin73"; // Dont change this or authentication things will break
-$api_CORS_origin = "https://coin73.nw.r.appspot.com/"; // automatically overwritten if we are on localdev unless et to public with "*";
+//$api_CORS_origin = "https://coin73.nw.r.appspot.com/"; // automatically overwritten if we are on localdev unless et to public with "*";
+$api_CORS_origin = "https://coin73.appspot.com/"; // automatically overwritten if we are on localdev unless et to public with "*";
 //$api_CORS_origin = "*"; // This makes the API interface public
 //$api_host = "http://api.coin73.nw.r.appspot.com/api/";
-$api_host = "https://coin73.nw.r.appspot.com/api/";
+//$api_host = "https://coin73.nw.r.appspot.com/api/";
+$api_host = "https://coin73.appspot.com/api/";
 $recaptcha_site_key = "6LddrUgcAAAAAEqZZuTgIZpDUT4KcNbqknFsCLyP";
 $recaptcha_secret_key = "6LddrUgcAAAAADqGmRqc85CRrJPXaPhUrfitAHnx";
 ?>
