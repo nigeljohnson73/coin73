93a0f0a096c95429e095c0e95eed34d432110dce
Nigel Johnson <nigel@nigeljohnson.net>
2021-09-18 22:08:33 +0100

    Fixed missing cron entries

--- www/_cron/every_minute.php
+++ www/_cron/every_minute.php
@@ -1,8 +1,15 @@
 <?php
 include_once (dirname ( __FILE__ ) . "/../functions.php");
+// Check for header: "X-Appengine-Cron: true"
 
-// Check for old miner jobs hanging around.
-// AOB
+// Remove any hung jobs
+// TODO: should probably go into system tick
+JobStore::tidyUp ();
+
+// TODO: Move this to the system tick handling so it's called every 10 seconds... once it's tuned
+transactionToBlock ();
+
+InfoStore::getInstance ()->setInfo ( cronMinuteDebugInfoKey (), "Completed: " . timestampFormat ( timestampNow (), "Y/m/d H:i:s" ) );
 
 echo "Minutely Housekeeing complete\n";
 ?>
