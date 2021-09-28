cc688c776de3e66b374e6e1bd300f48786ee8d8f
Nigel Johnson <nigel@nigeljohnson.net>
2021-09-28 19:54:33 +0100

    Better layout

--- www/_pages/signup.php
+++ www/_pages/signup.php
@@ -36,45 +36,45 @@
 			<br />
 
 			<form data-ng-show="!account_created && !account_not_created" novalidate>
-				<div class="row">
-					<div class="col-md-4">
-						<label for="email" class="form-label">Email address</label> <input type="email" class="form-control" id="email" data-ng-model="tx.email" data-ng-class="email_valid ? 'is-valid' : 'is-invalid'" data-ng-keyup="emailAddressValidate($event)" required>
-						<div class="valid-feedback">Looks good!</div>
+				<!-- RECAPTCHA progress -->
+				<div class="progress" data-ng-show="recaptcha_progress">
+					<div class="progress-bar progress-bar-striped progress-bar-animated" data-ng-class="recaptcha_progress<25 ? 'bg-danger' : recaptcha_progress<50 ? 'bg-warning' : 'bg-success'" role="progressbar" aria-valuenow="{{recaptcha_progress | number:0}}" aria-valuemin="0" aria-valuemax="100"
+						data-ng-style="{'width': recaptcha_progress + '%'}"></div>
+				</div>
+				<div class="shadow alert alert-secondary data-screen" role="alert">
+					<div class="row">
+						<div class="col-md-4">
+							<label for="email" class="form-label">Email address</label> <input type="email" class="form-control" id="email" data-ng-model="tx.email" data-ng-class="email_valid ? 'is-valid' : 'is-invalid'" data-ng-keyup="emailAddressValidate($event)" required>
+							<div class="valid-feedback">Looks good!</div>
+						</div>
+						<div class="col-md-4">
+							<label for="password" class="form-label">Password</label> <input type="password" class="form-control" id="password" data-ng-model="tx.password" data-ng-class="password_valid ? 'is-valid' : 'is-invalid'" data-ng-keyup="passwordValidate($event)" required>
+							<div class="valid-feedback">Looks good!</div>
+							<div class="invalid-feedback">8 characters, 1 uppercase, 1 lowercase, 1 digit and one of !@#$%^&amp;*</div>
+						</div>
+						<div class="col-md-4">
+							<label for="password_verify" class="form-label">Re-enter password</label> <input type="password" class="form-control" id="password_verify" data-ng-model="password_verify" data-ng-class="password_verify_valid ? 'is-valid' : 'is-invalid'" data-ng-change="passwordVerifyValidate($event)" required>
+							<div class="valid-feedback">Looks good!</div>
+							<div class="invalid-feedback">Must match valid password</div>
+						</div>
 					</div>
-					<div class="col-md-4">
-						<label for="password" class="form-label">Password</label> <input type="password" class="form-control" id="password" data-ng-model="tx.password" data-ng-class="password_valid ? 'is-valid' : 'is-invalid'" data-ng-keyup="passwordValidate($event)" required>
-						<div class="valid-feedback">Looks good!</div>
-						<div class="invalid-feedback">8 characters, 1 uppercase, 1 lowercase, 1 digit and one of !@#$%^&amp;*</div>
+					<div class="row">
+						<div class="col-12">
+							<input class="form-check-input" type="checkbox" value="" id="tocCheck" data-ng-model="tx.accept_toc" data-ng-class="tx.accept_toc ? 'is-valid' : 'is-invalid'" data-ng-change="tocValidate($event)" required> <label class="form-check-label" for="tocCheck"> I agree to the <a href="/terms" target="new">terms and
+									conditions</a></label>
+							<div class="invalid-feedback">You must agree before you will be given an account.</div>
+						</div>
 					</div>
-					<div class="col-md-4">
-						<label for="password_verify" class="form-label">Re-enter password</label> <input type="password" class="form-control" id="password_verify" data-ng-model="password_verify" data-ng-class="password_verify_valid ? 'is-valid' : 'is-invalid'" data-ng-change="passwordVerifyValidate($event)" required>
-						<div class="valid-feedback">Looks good!</div>
-						<div class="invalid-feedback">Must match valid password</div>
+					<div class="row">
+						<div class="col-12">
+							<button class="btn btn-custom" data-ng-disabled="!submittable" data-ng-click="requestAccount()" data-ng-hide="submitting">Request Account</button>
+							<img src="/gfx/ajax-loader-spinner.gif" alt="submitting" data-ng-show="submitting" />
+						</div>
 					</div>
 				</div>
-				<br />
-				<div class="row">
-					<div class="col-12">
-						<input class="form-check-input" type="checkbox" value="" id="tocCheck" data-ng-model="tx.accept_toc" data-ng-class="tx.accept_toc ? 'is-valid' : 'is-invalid'" data-ng-change="tocValidate($event)" required> <label class="form-check-label" for="tocCheck"> I agree to the <a href="/terms" target="new">terms and
-								conditions</a></label>
-						<div class="invalid-feedback">You must agree before you will be given an account.</div>
-					</div>
-				</div>
-				<div class="col-12">
-					<br />
-					<button class="btn btn-custom" data-ng-disabled="!submittable" data-ng-click="requestAccount()" data-ng-hide="submitting">Request Account</button>
-					<img src="/gfx/ajax-loader-spinner.gif" alt="submitting" data-ng-show="submitting" />
-				</div>
-
 			</form>
 		</div>
 
-		<!-- RECAPTCHA progress -->
-		<div class="progress" data-ng-show="recaptcha_progress">
-			<br />
-			<div class="progress-bar progress-bar-striped progress-bar-animated" data-ng-class="recaptcha_progress<25 ? 'bg-danger' : recaptcha_progress<50 ? 'bg-warning' : 'bg-success'" role="progressbar" aria-valuenow="{{recaptcha_progress | number:0}}" aria-valuemin="0" aria-valuemax="100"
-				data-ng-style="{'width': recaptcha_progress + '%'}"></div>
-		</div>
 
 		<div data-ng-show="account_created">
 			<div class="alert alert-success" role="alert">
